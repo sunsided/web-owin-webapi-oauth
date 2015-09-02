f8d43e3a912006620f6c3fd072cc3ab5456a958a
Markus Mayer <markus.mayer@derixx.de>
2015-09-02 18:37:53 +0200

    auth: Automatischen Test der Registrierung hinzugefügt

--- auth/Program.cs
+++ auth/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.Owin.Hosting;
+using Newtonsoft.Json;
 
 namespace auth
 {
@@ -27,7 +32,36 @@ namespace auth
             // Start the OWIN host
             using (WebApp.Start<Startup>(options))
             {
-                Console.ReadKey(true);
+                var baseAddress = $"http://localhost:{port}/";
+                var task = TestApi(baseAddress);
+                task.Wait();
+            }
+
+            Console.ReadKey(true);
+        }
+
+        /// <summary>
+        /// Tests the API.
+        /// </summary>
+        /// <param name="baseAddress">The base address.</param>
+        /// <returns>The task that represents this operation.</returns>
+        private static async Task TestApi([NotNull] string baseAddress)
+        {
+            // Create HttpCient and make a request to api/values
+            using (var client = new HttpClient())
+            {
+                var audienceModel = JsonConvert.SerializeObject(new
+                                                                    {
+                                                                        Name = "Something 9000"
+                                                                    });
+
+                var request = new StringContent(audienceModel, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(baseAddress + "api/audience", request);
+                Console.WriteLine(response);
+
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
             }
         }
     }
