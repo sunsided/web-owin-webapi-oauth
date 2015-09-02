cdad2944278824afdb5e232a9fcf7aa88eb0b8e3
Markus Mayer <markus.mayer@derixx.de>
2015-09-02 17:57:15 +0200

    auth: Startpunkt hinzugefügt

--- auth/Program.cs
+++ auth/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Owin.Hosting;
 
 namespace auth
 {
-    class Program
+    /// <summary>
+    /// Class Program.
+    /// </summary>
+    internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        internal static void Main([NotNull] string[] args)
         {
+            var options = new StartOptions();
+            const int port = 9001;
+
+            // NOTE: Bindungen müssen unter Windows explizit registriert werden. Symptomatisch für
+            // eine fehlende Registrierung ist das Auftreten einer TargetInvocationException.
+            // In diesem Fall muss folgender Befehl in einer Admin-Shell ausgeführt werden:
+            // netsh http add urlacl url=http://+:9001/ user=machine\username
+            options.Urls.Add($"http://+:{port}/");
+
+            // Start the OWIN host
+            using (WebApp.Start<Startup>(options))
+            {
+                Console.ReadKey(true);
+            }
         }
     }
 }
