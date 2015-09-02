2a56f667ca87c7eb3a6c41e3800464b9781452e5
Markus Mayer <markus.mayer@derixx.de>
2015-09-02 17:38:02 +0200

    auth: Mit Authorization Server begonnen

---
+++ auth/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using JetBrains.Annotations;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+namespace auth
+{
+    /// <summary>
+    /// OWIN / Web API 2 Startup
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Configures Web API.
+        /// </summary>
+        /// <remarks>
+        /// The Startup class is specified as a type parameter in the WebApp.Start method.
+        /// </remarks>
+        /// <param name="appBuilder">
+        /// The application builder.
+        /// </param>
+        public void Configuration([NotNull] IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            ConfigureOAuth(appBuilder);
+
+            appBuilder.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            appBuilder.UseWebApi(config);
+
+        }
+
+        /// <summary>
+        /// Configures OAuth.
+        /// </summary>
+        /// <param name="appBuilder">
+        /// The application builder.
+        /// </param>
+        public void ConfigureOAuth(IAppBuilder appBuilder)
+        {
+
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                // For Dev enviroment only (on production should be AllowInsecureHttp = false)
+#if DEBUG
+                AllowInsecureHttp = true,
+#else
+                AllowInsecureHttp = false,
+#endif
+                TokenEndpointPath = new PathString("/oauth2/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat("http://jwtauthzsrv.azurewebsites.net")
+            };
+
+            // OAuth 2.0 Bearer Access Token Generation
+            appBuilder.UseOAuthAuthorizationServer(OAuthServerOptions);
+        }
+    }
+}
