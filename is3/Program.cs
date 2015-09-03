99ab484237aae1d625ae5525d9e1fa3e4c8282d5
Markus Mayer <markus.mayer@derixx.de>
2015-09-03 17:08:01 +0200

    is3: Beispiel für IdentityServer 3 hinzugefügt

---
+++ is3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Serilog;
+
+namespace is3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "IdentityServer3 SelfHost";
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo
+                .LiterateConsole(outputTemplate: "{Timestamp:HH:MM} [{Level}] ({Name:l}){NewLine} {Message}{NewLine}{Exception}")
+                .CreateLogger();
+
+            const string url = "https://localhost:44333/core";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("\n\nServer listening at {0}. Press enter to stop", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ is3/Startup.cs
@@ -0,0 +1,28 @@
+﻿using is3.Config;
+using IdentityServer3.Core.Configuration;
+using Owin;
+
+namespace is3
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var factory = new IdentityServerServiceFactory();
+            factory
+                .UseInMemoryClients(Clients.Get())
+                .UseInMemoryScopes(Scopes.Get())
+                .UseInMemoryUsers(Users.Get());
+
+            var options = new IdentityServerOptions
+            {
+                SiteName = "IdentityServer3 (self host)",
+
+                SigningCertificate = Certificate.Get(),
+                Factory = factory,
+            };
+
+            appBuilder.UseIdentityServer(options);
+        }
+    }
+}
